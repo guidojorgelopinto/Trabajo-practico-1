6b03255482990bd211994b807c87c318757f5cf0
Guido <guidojorgelopinto@gmail.com>
2021-05-05 21:04:44 -0300

    7mo commit

--- scripts/index.js
+++ scripts/index.js
@@ -18,17 +18,17 @@ function topFunction() {
 }
 
 
-// -------------slides
+// -------------slider
 
 var slideIndex = 1;
-showSlides(slideIndex);
+if (typeof showSlides !== 'reload');
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
