6048d79833802e5a7b39fc082d3327f3b077f98f
Guido <guidojorgelopinto@gmail.com>
2021-05-06 17:06:47 -0300

    12 commit

--- scripts/index.js
+++ scripts/index.js
@@ -36,8 +36,30 @@ function topFunction() {
 window.onscroll = () => {
   if (
     document.documentElement.scrollTop >
-    document.documentElement.clientHeight * 0.7
+    document.documentElement.clientHeight * 0.10
   ) {
     promociones.classList.add("effect1");
   }
 };
+
+// -------------slider
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
